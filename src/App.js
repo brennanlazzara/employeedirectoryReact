7ac946cdc9138e0e4c2aa90b00bad3d0e5d72a49
Brennan Predmore <brennnann@gmail.com>
2020-04-20 13:32:35 -0500

    bootstrap cdn init // npm i reactstrap // header.js/table.js init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from './Header';
+import Table from './Table'
 
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Table />>
     </div>
   );
 }
