ebf2e7ac323fa33afc1da0333d6a9976b8509f60
Brennan Predmore <brennnann@gmail.com>
2020-04-20 14:07:12 -0500

    npm i axios // axios get request init with URL from https://randomuser.me/

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Header from './Header';
 import Table from './Table'
 import Search from './Search'
+import axios from 'axios'
 
 import './App.css';
 
 
 function App() {
+  useEffect(()=> {  
+    // Make a request for a user with a given ID
+    axios.get('https://randomuser.me/api/')
+.then(function (response) {
+  // handle success
+  console.log(response);
+})
+.catch(function (error) {
+  // handle error
+  console.log(error);
+})
+.then(function () {
+  // always executed
+});
+  }) 
+
+
   return (
     <div className="App">
       <Header />
